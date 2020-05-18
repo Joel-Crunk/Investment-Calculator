5f097fb85950b76d371c5ec5b804df4b0accdbd7
Joel-Crunk <joel.crunk@outlook.com>
2020-05-18 19:00:00 -0400

    Add project files.

---
+++ ConsoleApp70/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp70
+{ 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many interations (months or Years)? ");
+            double Num2 = Convert.ToDouble(Console.ReadLine());
+
+
+        Console.WriteLine("Original amount? ");
+            double amountOriginal = Convert.ToDouble(Console.ReadLine());
+            
+
+            Console.WriteLine("What interst rate? ");
+            double rate = Convert.ToDouble(Console.ReadLine());
+
+
+        Console.WriteLine("Amount added each interation? ");
+            double increase = Convert.ToDouble(Console.ReadLine());
+
+
+        double A = 0;
+            for (int i = 0; i < Num2; i++)
+            {
+                
+                 A = (A + amountOriginal) * rate + increase +A;
+
+            }
+            double b = A + amountOriginal;
+           Console.WriteLine( b);
+            
+            
+
+            
+        }
+    }
+}
